12735adcd2ccc6fcc97d1c799e019545e170fad0
Flow <80765567+Dinkelmeme@users.noreply.github.com>
2022-06-23 16:08:05 +0200

    Update FlowDownloader.py

--- FlowDownloader.py
+++ FlowDownloader.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 
 from random import randint
 from pytube.cli import on_progress
-
+# Ascii art \/
 print("""\
   ______ _                 _____                      _                 _           
  |  ____| |               |  __ \                    | |               | |          
@@ -13,22 +13,26 @@ print("""\
  |_|    |_|\___/ \_/\_/   |_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|\___|_|                                                                                                                                                                       
                           The Fastest Youtube Downloader      Made by Erik Hellak
                     """)
-
+# Asking for Link input
 link = input("Flow Downloader: Enter Youtube link:")
 yt = YouTube(link)
 
 print("Flow Downloader: Downloading from YouTube...")
 
+# Searching for Video on Youtube
 yt=YouTube(link,on_progress_callback=on_progress)
 videos=yt.streams.first()
 videos.download()
 print("(:")
-     
+  
+# downloading Video in Highest Res
 ys = yt.streams.get_highest_resolution()
 ys.download()
 print(" ")
 print("Flow Downloader: Download Completed!")
 
+
+# Auto Closing the Programm
 n = 100
 while n > 0:
     n -= 1
