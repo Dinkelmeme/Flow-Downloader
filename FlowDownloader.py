5b49265cea6c62a94d16b22869ba6720fc71e4f0
Flow <80765567+Dinkelmeme@users.noreply.github.com>
2022-06-23 15:57:23 +0200

    Add files via upload
    .py code and executable file (python must be installed)

---
+++ FlowDownloader.py
@@ -0,0 +1,37 @@
+from asyncio import wait_for
+from pytube import YouTube
+
+from random import randint
+from pytube.cli import on_progress
+
+print("""\
+  ______ _                 _____                      _                 _           
+ |  ____| |               |  __ \                    | |               | |          
+ | |__  | | _____      __ | |  | | _____      ___ __ | | ___   __ _  __| | ___ _ __ 
+ |  __| | |/ _ \ \ /\ / / | |  | |/ _ \ \ /\ / / '_ \| |/ _ \ / _` |/ _` |/ _ \ '__|
+ | |    | | (_) \ V  V /  | |__| | (_) \ V  V /| | | | | (_) | (_| | (_| |  __/ |   
+ |_|    |_|\___/ \_/\_/   |_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|\___|_|                                                                                                                                                                       
+                          The Fastest Youtube Downloader      Made by Erik Hellak
+                    """)
+
+link = input("Flow Downloader: Enter Youtube link:")
+yt = YouTube(link)
+
+print("Flow Downloader: Downloading from YouTube...")
+
+yt=YouTube(link,on_progress_callback=on_progress)
+videos=yt.streams.first()
+videos.download()
+print("(:")
+     
+ys = yt.streams.get_highest_resolution()
+ys.download()
+print(" ")
+print("Flow Downloader: Download Completed!")
+
+n = 100
+while n > 0:
+    n -= 1
+print("Bye...")
+
+exit()
